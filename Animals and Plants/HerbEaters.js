a7c1e800e6605cc016692eba5c4ea40a11773797
Horesz <86825984+Horesz@users.noreply.github.com>
2024-07-01 10:57:49 +0200

    update HerbEaters.js

---
+++ Animals and Plants/HerbEaters.js
@@ -0,0 +1,22 @@
+//Növényevők 
+import Creature from './Creature.js';
+
+class HerbEater extends Creature {
+    constructor(name) {
+        super();
+        this.name = name;
+        this.food = "plants"; 
+    }
+
+    eating() {
+        console.log(`${this.name} Éppen eszik egy kis ${this.food}`);
+    }
+
+    moving() {
+        console.log(`${this.name} Mozgásban van`);
+    }
+
+    isDead() {
+        return false;
+    }
+}
