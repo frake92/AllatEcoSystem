08834552c28c74fb155deeb9e37873e9780d57bd
jani <bartusjani004@gmail.com>
2024-07-01 11:54:44 +0200

    Carnivores.js

---
+++ Animals and Plants/Carnivores.js
@@ -0,0 +1,18 @@
+import Creatures from './Creatures.js';
+
+class Carnivores extends Creatures {
+    constructor(name) {
+        super();
+        this.name = name;
+        this.food = "meat";
+    }
+    eating() {
+        console.log(`${this.name} is eating some ${this.food}`);
+    }
+    moving() {
+        console.log(`${this.name} is moving`);
+    }
+    isDead() {
+        return false;
+    }
+}
