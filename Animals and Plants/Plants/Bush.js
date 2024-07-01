7d0baa1363d5847796282c1dc07a639da02c50a2
jani <bartusjani004@gmail.com>
2024-07-01 10:52:16 +0200

    Bush.js

---
+++ Animals and Plants/Plants/Bush.js
@@ -0,0 +1,8 @@
+class Bush extends Plants {
+    constructor(type) {
+        super(type);
+    }
+    eaten() {
+        console.log('The bush was eaten');
+    }
+}
--- Animals and Plants/Plants/Grass.js
+++ Animals and Plants/Plants/Grass.js
@@ -1,7 +1,6 @@
 class Grass extends Plants {
     constructor(type) {
         super(type);
-        this.color = color;
     }
     eaten() {
         console.log('The grass was eaten');
